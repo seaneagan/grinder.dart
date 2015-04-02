fb34279b19c03c9d85b100f4efa1c1dcdabbb69e
Sean Eagan <seaneagan1@gmail.com>
2015-04-02 14:10:09 -0500

    WIP

--- lib/src/cli.dart
+++ lib/src/cli.dart
@@ -7,14 +7,14 @@ import 'dart:async';
 import 'dart:convert' show JSON, UTF8;
 import 'dart:io';
 
-import 'package:args/args.dart';
+import 'package:unscripted/unscripted.dart';
 
 import 'singleton.dart';
 import 'utils.dart';
 import '../grinder.dart';
 
 // This version must be updated in tandem with the pubspec version.
-const String APP_VERSION = '0.6.6+3';
+const String APP_VERSION = '0.7.0-dev.1';
 
 List<String> grinderArgs() => _args;
 List<String> _args;
@@ -22,64 +22,47 @@ List<String> _args;
 Future handleArgs(List<String> args) {
   _args = args == null ? [] : args;
 
-  ArgParser parser = createArgsParser();
-  ArgResults results = parser.parse(grinderArgs());
+  cli(
+    List<String> tasks,
+    {@Flag(help: 'Print the version of grinder.')
+     bool version,
+     @Flag(help: 'Print the dependencies of tasks.')
+     bool deps
+    }) {
 
-  if (results['version']) {
-    const String pubUrl = 'https://pub.dartlang.org/packages/grinder.json';
+    if (version) {
+      const String pubUrl = 'https://pub.dartlang.org/packages/grinder.json';
 
-    print('grinder version ${APP_VERSION}');
+      print('grinder version ${APP_VERSION}');
 
-    return httpGet(pubUrl).then((String str) {
-      List versions = JSON.decode(str)['versions'];
-      if (APP_VERSION != versions.last) {
-        print("Version ${versions.last} is available! Run `pub global activate"
-            " grinder` to get the latest version.");
-      } else {
-        print('grinder is up to date!');
-      }
-    }).catchError((e) => null);
-  } else if (results['help']) {
-    printUsage(parser, grinder);
-  } else if (results['deps']) {
-    printDeps(grinder);
-  } else {
-    Future result = grinder.start(results.rest);
-
-    return result.catchError((e, st) {
-      if (st != null) {
-        print('\n${e}\n${st}');
-      } else {
-        print('\n${e}');
-      }
-      exit(1);
-    });
+      return httpGet(pubUrl).then((String str) {
+        List versions = JSON.decode(str)['versions'];
+        if (APP_VERSION != versions.last) {
+          print("Version ${versions.last} is available! Run `pub global activate"
+              " grinder` to get the latest version.");
+        } else {
+          print('grinder is up to date!');
+        }
+      }).catchError((e) => null);
+    } else if (deps) {
+      printDeps(grinder);
+    } else {
+      return grinder.start(tasks).catchError((e, st) {
+        if (st != null) {
+          print('\n${e}\n${st}');
+        } else {
+          print('\n${e}');
+        }
+        exit(1);
+      });
+    }
   }
 
-  return new Future.value();
+  return declare(cli).execute(grinderArgs());
 }
 
-ArgParser createArgsParser() {
-  ArgParser parser = new ArgParser();
-  parser.addFlag('version', negatable: false,
-      help: "print the version of grinder");
-  parser.addFlag('help', abbr: 'h', negatable: false,
-      help: "show targets but don't build");
-  parser.addFlag('deps', abbr: 'd', negatable: false,
-      help: "display the dependencies of targets");
-  return parser;
-}
-
-void printUsage(ArgParser parser, Grinder grinder) {
-  print('usage: dart ${currentScript()} <options> target1 target2 ...');
-  print('');
-  print('valid options:');
-  print(parser.usage.replaceAll('\n\n', '\n'));
-
+void printUsage(Grinder grinder) {
   if (!grinder.tasks.isEmpty) {
-    print('');
-    print('valid targets:');
-
     List<GrinderTask> tasks = grinder.tasks.toList();
     tasks.forEach((t) {
       var buffer = new StringBuffer()..write('  $t');
@@ -90,23 +73,14 @@ void printUsage(ArgParser parser, Grinder grinder) {
   }
 }
 
-String currentScript() {
-  String script = Platform.script.toString();
-  String uriBase = Uri.base.toString();
-  if (script.startsWith(uriBase)) {
-    script = script.substring(uriBase.length);
-  }
-  return script;
-}
-
 void printDeps(Grinder grinder) {
   // calculate the dependencies
   grinder.start([], dontRun: true);
 
   if (grinder.tasks.isEmpty) {
-    print("no grinder targets defined");
+    print("no grinder tasks defined");
   } else {
-    print('grinder targets:');
+    print('grinder tasks:');
     print('');
 
     List<GrinderTask> tasks = grinder.tasks.toList();
